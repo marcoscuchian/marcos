3c9386462268001990607d6183bbc4a76ba82af6
Franco Gomez <gomez_f@outlook.com>
2019-05-16 21:09:43 -0300

    terminado de mesas- restorant

--- restorant/js/app.js
+++ restorant/js/app.js
@@ -1,21 +1,40 @@
 // variables globales o principales//
 var listaMesas = [];
 var contadorMesas=0;
-
-
+var estados = ["Libre","Reservada","Ocupada"];
 // funcion para mostrar todas las mesas//
 function mostrarMesas(){
     var html = '';
 
     for(var i=0 ; i< listaMesas.length ; i++) { 
+
+        var clase = "";
+        var clase2= "";
+        var clase3 = "";
+
+        if(listaMesas[i].estado == 1 ) {
+            clase = "d-none"; 
+        }
+        
+        else if(listaMesas[i].estado ==2){
+            clase2 ="d-none";
+        }
+        else{
+            clase3="d-none";
+        }
         var mesaNueva=
         '<div class="col-sm-12 col-md-4 col-lg-3">'+
-            '<div class="card" style="width: 18rem;">'+
+            '<div class="card my-2" style="width: 18rem;">'+
+                '<button type="button" class="close"  aria-label="Close" onclick="borrarMesa('+i+')">' +
+                    '<span aria-hidden="true">&times;</span>'+
+                '</button>'+
                 '<div class="card-body">'+
                     '<h5 class="card-title">MESA #'+ listaMesas[i].numeroMesa +'</h5>'+
-                    '<h6 class="card-subtitle mb-2 text-muted">ESTADO</h6>'+
-                    '<a href="#" class="card-link">Reservar</a>'+
-                    '<a href="#" class="card-link">Ocupada</a>'+
+                    '<h6 class="card-subtitle mb-2 text-muted">'+ estados[listaMesas[i].estado] +'</h6>'+
+                    '<a href="#" class="card-link '+ clase+' '+ clase2+'" onclick="cambiarEstado('+i+',1)">Reservar </a>'+
+                    '<a href="#" class="card-link '+ clase2+'" onclick="cambiarEstado('+i+',2)">Ocupar</a>'+
+                    '<a href="#" class="card-link '+ clase3+'" onclick="cambiarEstado('+i+',0)">LIBERAR</a>'+
+
                 '</div>'+
             '</div>'+
         '</div>';
@@ -27,6 +46,13 @@ function mostrarMesas(){
     document.querySelector("#grillaMesas").innerHTML = html;
 
 }
+
+function cambiarEstado(posicion,estado){
+    listaMesas[posicion].cambiarEstado(estado);
+    mostrarMesas();
+
+}
+
 // funcion para agregar mesa//
 function agregarMesa(){
     contadorMesas++;
@@ -34,7 +60,17 @@ function agregarMesa(){
     listaMesas.push(nuevamesa);
     mostrarMesas();
 }
-// crear una mesa//
+
+function borrarMesa(posicion){
+    listaMesas.splice(posicion,1);
+    mostrarMesas();
+}
+
+
+
+
+
+// crear una mesa es un molde siempre va al ultimo//
 class Mesa { 
     estado;
     numeroMesa;
@@ -54,4 +90,7 @@ class Mesa {
         this.estado = estadoNuevo;
     }
 
+
+
 }
+
